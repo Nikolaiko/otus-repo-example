7cb7321f56fe5618845dea37a77af774c5d45cf6
Nikolai Baklanov <nikolaiko@mail.ru>
2023-11-21 23:55:02 +0600

    Initial commit

---
+++ ExampleProject/ExampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleProjectApp.swift
+//  ExampleProject
+//
+//  Created by Nikolai Baklanov on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
