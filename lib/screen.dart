65138a1fdf619ee575efda5b22ae6ec443472c8b
Nikolai Baklanov <nikolaiko@mail.ru>
2024-01-29 11:17:54 +0600

    dz2

--- lib/screen.dart
+++ lib/screen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/service.dart';
 
 class MyWidget extends StatelessWidget {
   const MyWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Privet!"),);
+    return Center(child: Text(Service().getData()),);
   }
 }
---
+++ lib/service.dart
@@ -0,0 +1,5 @@
+class Service {
+  String getData() {
+    return "MyData";
+  }
+}
